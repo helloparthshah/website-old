b61c6f94b168ac4528054d89b9bd044016035aa8
Parth Shah <helloparthshah@gmail.com>
2020-05-17 21:22:21 -0700

    Moved team to new page

--- DSC/app.js
+++ DSC/app.js
@@ -41,6 +41,19 @@ window.addEventListener('DOMContentLoaded', init)
 
 const sliders = document.querySelectorAll('.slide-in');
 
+function scrl1() {
+    const nav = document.querySelector('#nbr');
+    var test = document.querySelector('html');
+
+    if (test.scrollTop <= 100 && nav.className === "navbar") {
+        nav.style.backgroundColor = 'rgba(255,255,255,0)';
+        nav.style.boxShadow = 'none';
+    } else {
+        nav.style.backgroundColor = 'rgba(255,255,255,0.9)';
+        nav.style.boxShadow = '0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2)';
+    }
+}
+
 function scrl() {
 
     const nav = document.querySelector('#nbr');
